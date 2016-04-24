7563576056bb257c0f1b4ee2fa44f3c278821c71
Zane Clifton <Zane Clifton>
2016-04-24 11:49:55 -0500

    code for cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix assigns cached variables 'x' and 'm'
+## 	through its return functions
+##		$set - takes input matrix as its argument and assigns it to 'x'
+##		$get - returns 'x'
+##		$setsolve - assigns inverse matrix to 'm'
+##		$getsolve - returns 'm'
+## cacheSolve calculates the inverse and assigns it to 'm', if 'm' is NULL
+##	returns 'm' - if it exists or if calculated
+##	main input argument is the assigned output from calling makeCacheMatrix()
 
-## Write a short comment describing this function
+
+## returns a set of functions to assign (environment) variables 'x' and 'm'
+## 'x' is the input matrix; 'm' is its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y     #y is the input matrix
+        m <<- NULL  #m is the inverse, which is nothing yet
+    }
+    get <- function() x  #this is the matrix assigned by 'set'
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## computes inverse of matrix, if it's not already cached,
+## and assigns inverse to (environment) variable 'm'
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(f, ...) {
+    ## Return a matrix that is the inverse of 'x'
+    m <- f$getsolve()
+    #if inverse exists - !is.null - return it along with a message
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    #otherwise - create the inverse; assign it to 'm'; and return it
+    data <- f$get()  #returns the input matrix ('x') from makeCacheMatrix
+    m <- solve(data) #create the inverse
+    f$setsolve(m)    #assign (cache) inverse to 'm'
+    m                #return 'm'
 }
